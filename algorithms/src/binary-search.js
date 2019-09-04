fd5ca7d5cce46bcf2afe5ce454e3e31fd1b58f9e
Brian Mancini <bmancini@gmail.com>
2019-09-04 09:26:19 -0400

    implemented binary search and binary search upper and lower bounds

---
+++ algorithms/src/binary-search.js
@@ -0,0 +1,82 @@
+module.exports.search = search;
+module.exports.lowerBound = lowerBound;
+module.exports.upperBound = upperBound;
+
+/**
+ * Finds the index of the target value in the array. Returns -1
+ * if the value is not found.
+ *
+ * @param {number[]} a sorted array
+ * @param {number} l left index
+ * @param {number} r right index
+ * @param {number} x target value
+ * @returns {number} the index of the value in the array
+ */
+function search(a, l, r, x) {
+	if (l <= r) {
+		let mid = l + Math.floor((r - l) / 2);
+		if (a[mid] === x) return mid;
+		if (a[mid] > x) return search(a, l, mid - 1, x);
+		if (a[mid] < x) return search(a, mid + 1, r, x);
+	}
+	return -1;
+}
+
+/**
+ * With a sorted array, returns the index of the first value
+ * that is greather than or equal to the search value x.
+ *
+ * Returns 0 when entire array is above the value.
+ * Returns PositiveInfinity when value is above entire array.
+ *
+ * @param {number[]} a sorted array
+ * @param {number} l left index
+ * @param {number} r right index
+ * @param {number} x target value
+ * @returns {number} index of first value above the target
+ */
+function lowerBound(a, l, r, x) {
+	// return Infinity when x is above array
+	if (x > a[a.length - 1]) return Number.POSITIVE_INFINITY;
+
+	// return first index when x is below array
+	if (x < a[0]) return 0;
+
+	while (l < r) {
+		let mid = l + Math.floor((r - l) / 2);
+		if (a[mid] === x) return mid;
+		if (a[mid] < x) l = mid + 1;
+		if (a[mid] > x) r = mid;
+	}
+	return l;
+}
+
+/**
+ * With a sorted array, returns the index of the last value
+ * that is less than or equal to the search value x.
+ *
+ * Returns -1 when entire array is above the value.
+ * Returns last index when entire array is below the value.
+ *
+ * @param {number[]} a array of values
+ * @param {number} l left index
+ * @param {number} r right index
+ * @param {number} x search value
+ * @returns index of position <= x
+ */
+function upperBound(a, l, r, x) {
+	// return - 1 when x is below array
+	if (x < a[0]) return -1;
+
+	// return last index when x is above array
+	if (x > a[a.length - 1]) return a.length - 1;
+
+	// shrink array until we find the value
+	while (l < r) {
+		let mid = l + Math.floor((r - l) / 2);
+		if (a[mid] === x) return mid;
+		if (a[mid] < x) l = mid + 1;
+		if (a[mid] > x) r = mid;
+	}
+	return l - 1;
+}
---
+++ algorithms/test/binary-search.spec.js
@@ -0,0 +1,134 @@
+const { expect } = require('chai');
+const { search } = require('../src/binary-search');
+const { lowerBound } = require('../src/binary-search');
+const { upperBound } = require('../src/binary-search');
+
+describe('binary search', () => {
+	describe('.search', () => {
+		it('should return -1 when below', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 0;
+			expect(search(a, 0, a.length, x)).to.equal(-1);
+		});
+
+		it('should return -1 when not found', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 3;
+			expect(search(a, 0, a.length, x)).to.equal(-1);
+		});
+
+		it('should return first value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 1;
+			expect(search(a, 0, a.length, x)).to.equal(0);
+		});
+
+		it('should return mid value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 4;
+			expect(search(a, 0, a.length, x)).to.equal(1);
+		});
+
+		it('should return mid value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 8;
+			expect(search(a, 0, a.length, x)).to.equal(2);
+		});
+
+		it('should return mid value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 12;
+			expect(search(a, 0, a.length, x)).to.equal(3);
+		});
+
+		it('should return last value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 21;
+			expect(search(a, 0, a.length, x)).to.equal(4);
+		});
+
+		it('should return -1 above', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 22;
+			expect(search(a, 0, a.length, x)).to.equal(-1);
+		});
+	});
+
+	describe('.lowerBound', () => {
+		it('should return 0 when below the array', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 0;
+			expect(lowerBound(a, 0, a.length, x)).to.equal(0);
+		});
+
+		it('should return first position', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 1;
+			expect(lowerBound(a, 0, a.length, x)).to.equal(0);
+		});
+
+		it('should return values above missing value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 5;
+			expect(lowerBound(a, 0, a.length, x)).to.equal(2);
+		});
+
+		it('should return values at a number', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 8;
+			expect(lowerBound(a, 0, a.length, x)).to.equal(2);
+		});
+
+		it('should return values at last position', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 21;
+			expect(lowerBound(a, 0, a.length, x)).to.equal(4);
+		});
+
+		it('should return PositiveInfinity when above the array', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 22;
+			expect(lowerBound(a, 0, a.length, x)).to.equal(
+				Number.POSITIVE_INFINITY
+			);
+		});
+	});
+
+	describe('.upperBound', () => {
+		it('should return -1 when before start', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 0;
+			expect(upperBound(a, 0, a.length, x)).to.equal(-1);
+		});
+
+		it('should return hit on first', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 1;
+			expect(upperBound(a, 0, a.length, x)).to.equal(0);
+		});
+
+		it('should return values below a threshold', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 15;
+			expect(upperBound(a, 0, a.length, x)).to.equal(3);
+		});
+
+		it('should return values at a threshold', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 12;
+			expect(upperBound(a, 0, a.length, x)).to.equal(3);
+		});
+
+		it('should return values at last position', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 21;
+			expect(upperBound(a, 0, a.length, x)).to.equal(4);
+		});
+
+		it('should return all values when above last position', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 25;
+			expect(upperBound(a, 0, a.length, x)).to.equal(4);
+		});
+	});
+});
