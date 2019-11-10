1c1eb4d42d5d535b59352a3c85b45c1ebc24054b
Brian Mancini <bmancini@gmail.com>
2019-11-10 15:53:24 -0500

    knapsack problem completion

---
+++ algorithms/problems/problem-knapsack.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { knapsack } = require('../src/knapsack');
+
+// const input = './input/knapsack1_10.txt'; // 147
+// const input = './input/knapsack1_1000.txt'; // 178629
+// const input = './input/knapsack1.txt'; // 2493893
+const input = './input/knapsack2.txt'; // 4243395
+
+const raw = fs.readFileSync(input, 'utf8');
+
+const data = raw
+	.split('\n')
+	.filter(p => p)
+	.map(line => line.split(' ').map(Number));
+
+const [size, itemCnt] = data[0];
+
+const items = data.slice(1);
+
+console.log('size', size, 'items', itemCnt);
+console.log('value', knapsack(items, size));
--- algorithms/src/knapsack.js
+++ algorithms/src/knapsack.js
@@ -1,8 +1,16 @@
 module.exports = {
-	knapsack,
+	knapsack: knapsackOptimized,
+	knapsackNaive,
+	knapsackOptimized,
+	knapsackRecursive,
 };
 
-function knapsack(items, maxSize) {
+function knapsackNaive(items, maxSize) {
+	const safeGet = (v, i, j) => {
+		if (v[i] == undefined || v[i][j] == undefined) return 0;
+		return v[i][j];
+	};
+
 	let v = new Array(items.length);
 
 	// iterate all items
@@ -28,7 +36,63 @@ function knapsack(items, maxSize) {
 	return v[items.length - 1][maxSize];
 }
 
-function safeGet(v, i, j) {
-	if (v[i] == undefined || v[i][j] == undefined) return 0;
-	return v[i][j];
+function knapsackOptimized(items, maxSize) {
+	const safeGet = (v, s) => v[s] || 0;
+
+	let vp = new Array(maxSize + 1);
+	let v = new Array(maxSize + 1);
+
+	// iterate all items
+	for (let i = 0; i < items.length; i++) {
+		let vi = items[i][0]; // value of i
+		let si = items[i][1]; // size of i
+
+		for (let s = 0; s <= maxSize; s++) {
+			if (si > s) {
+				// case 1
+				v[s] = safeGet(vp, s);
+			} else {
+				// case 2
+				v[s] = Math.max(
+					safeGet(vp, s), // case 1
+					safeGet(vp, s - si) + vi // case 2
+				);
+			}
+		}
+		let temp = vp;
+		vp = v;
+		v = temp;
+	}
+	return vp[maxSize];
+}
+
+function knapsackRecursive(items, maxSize) {
+	let vals = new Array(items.length + 1);
+	for (let i = 0; i < vals.length; i++) {
+		vals[i] = new Array(maxSize + 1);
+	}
+
+	let calc = (i, s) => {
+		if (i === 0 || s <= 0) return 0;
+
+		let vi = items[i - 1][0]; // value of i
+		let si = items[i - 1][1]; // size of i
+
+		if (vals[i - 1][s] === undefined) {
+			vals[i - 1][s] = calc(i - 1, s);
+		}
+
+		if (si > s) vals[i][s] = vals[i - 1][s];
+		else {
+			if (vals[i - 1][s - si] === undefined) {
+				vals[i - 1][s - si] = calc(i - 1, s - si);
+			}
+			vals[i][s] = Math.max(vals[i - 1][s], vals[i - 1][s - si] + vi);
+		}
+
+		// console.log(i, s, vi, items[i], vals[i]);
+		return vals[i][s];
+	};
+
+	return calc(items.length, maxSize);
 }
