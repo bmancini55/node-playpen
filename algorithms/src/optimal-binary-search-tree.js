312fcd587bd2542fdd7f15b6ec0e9c2b0c7a41a8
Brian Mancini <bmancini@gmail.com>
2019-11-24 10:17:17 -0500

    creating obst algorithm

--- algorithms/src/optimal-binary-search-tree.js
+++ algorithms/src/optimal-binary-search-tree.js
@@ -1,5 +1,6 @@
 module.exports = {
 	obstWeight,
+	obst,
 };
 
 function sum(a) {
@@ -31,4 +32,45 @@ function obstWeight(keys, probs) {
 		}
 	}
 	return map.get(key(0, n - 1));
+}
+
+function obst(keys, probs) {
+	let weights = new Map();
+	let roots = new Map();
+	let n = keys.length;
+	for (let s = 1; s <= n; s++) {
+		for (let t = 0; t <= n - s; t++) {
+			let ti = t;
+			let tj = ti + s - 1;
+
+			let sumProb = sum(probs.slice(ti, tj + 1));
+			let minRootWeight = Number.POSITIVE_INFINITY;
+			let minRoot;
+			for (let r = ti; r <= tj; r += 1) {
+				let tl = weights.get(key(ti, r - 1)) || 0;
+				let tr = weights.get(key(r + 1, tj)) || 0;
+				if (tl + tr < minRootWeight) {
+					minRootWeight = tl + tr;
+					minRoot = r;
+				}
+				// console.log(`n: ${n} s: ${s} t: ${t} r: ${r} ti: ${ti} tj: ${tj} tl: ${tl} tr: ${tr} sum: ${sumProb}`); // prettier-ignore
+			}
+			roots.set(key(ti, tj), minRoot);
+			weights.set(key(ti, tj), sumProb + minRootWeight);
+		}
+	}
+
+	return constructObst(keys, roots, 0, n - 1);
+}
+
+function constructObst(keys, rootMap, i, j) {
+	let index = rootMap.get(key(i, j));
+	if (index === undefined) return;
+
+	let root = {
+		value: keys[index],
+		left: constructObst(keys, rootMap, i, index - 1),
+		right: constructObst(keys, rootMap, index + 1, j),
+	};
+	return root;
 }
--- algorithms/test/optimal-binary-search-tree.spec.js
+++ algorithms/test/optimal-binary-search-tree.spec.js
@@ -1,34 +1,60 @@
 const { expect } = require('chai');
-const { obstWeight } = require('../src/optimal-binary-search-tree');
+const { obstWeight, obst } = require('../src/optimal-binary-search-tree');
 
-describe('obst', () => {
-	it('should return weight for a 1 node tree', () => {
-		let keys = [1];
-		let probs = [0.2];
-		expect(obstWeight(keys, probs)).to.closeTo(0.2, 0.01);
+describe('Optimal Binary Search Tree', () => {
+	describe('obstWeight', () => {
+		it('should return weight for a 1 node tree', () => {
+			let keys = [1];
+			let probs = [0.2];
+			expect(obstWeight(keys, probs)).to.closeTo(0.2, 0.01);
+		});
+
+		it('should return weight for a 2 node tree', () => {
+			let keys = [1, 2];
+			let probs = [0.2, 0.1];
+			expect(obstWeight(keys, probs)).to.closeTo(0.4, 0.01);
+		});
+
+		it('should return weight for a 3 node tree', () => {
+			let keys = [1, 2, 3];
+			let probs = [0.2, 0.1, 0.4];
+			expect(obstWeight(keys, probs)).to.be.closeTo(1.1, 0.01);
+		});
+
+		it('should return weight for a 4 node tree', () => {
+			let keys = [1, 2, 3, 4];
+			let probs = [0.2, 0.1, 0.4, 0.3];
+			expect(obstWeight(keys, probs)).to.be.closeTo(1.7, 0.01);
+		});
+
+		it('should return weight of larger', () => {
+			let keys = [1, 2, 3, 4, 5, 6, 7];
+			let probs = [0.2, 0.05, 0.17, 0.1, 0.2, 0.03, 0.25];
+			expect(obstWeight(keys, probs)).to.be.closeTo(2.23, 0.01);
+		});
 	});
 
-	it('should return weight for a 2 node tree', () => {
-		let keys = [1, 2];
-		let probs = [0.2, 0.1];
-		expect(obstWeight(keys, probs)).to.closeTo(0.4, 0.01);
-	});
-
-	it('should return weight for a 3 node tree', () => {
-		let keys = [1, 2, 3];
-		let probs = [0.2, 0.1, 0.4];
-		expect(obstWeight(keys, probs)).to.be.closeTo(1.1, 0.01);
-	});
-
-	it('should return weight for a 4 node tree', () => {
-		let keys = [1, 2, 3, 4];
-		let probs = [0.2, 0.1, 0.4, 0.3];
-		expect(obstWeight(keys, probs)).to.be.closeTo(1.7, 0.01);
-	});
-
-	it('should return weight of larger', () => {
-		let keys = [1, 2, 3, 4, 5, 6, 7];
-		let probs = [0.2, 0.05, 0.17, 0.1, 0.2, 0.03, 0.25];
-		expect(obstWeight(keys, probs)).to.be.closeTo(2.23, 0.01);
+	describe('obst', () => {
+		it('should return single node', () => {
+			let keys = [1, 2, 3, 4];
+			let probs = [0.2, 0.1, 0.4, 0.3];
+			expect(obst(keys, probs)).to.deep.equal({
+				value: 3,
+				left: {
+					value: 1,
+					left: undefined,
+					right: {
+						value: 2,
+						left: undefined,
+						right: undefined,
+					},
+				},
+				right: {
+					value: 4,
+					left: undefined,
+					right: undefined,
+				},
+			});
+		});
 	});
 });
