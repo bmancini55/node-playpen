c46a2aba32ee883ad0d22d98e5a1e07ea91c2a76
Brian Mancini <bmancini@gmail.com>
2020-03-01 19:48:07 -0500

    adding fast fib method

--- algorithms/src/math/fibonacci.js
+++ algorithms/src/math/fibonacci.js
@@ -1,4 +1,3 @@
-const bint = require('bignumber.js');
 const matrix = require('./matrix');
 
 module.exports = {
@@ -12,16 +11,16 @@ module.exports = {
  * This algorithm takes advantage of the recurrence F(n) = F(n-2) + F(n-1).
  * Space complexity O(1)
  * Time complexity O(n)
- * @param {BigNumber} n
- * @returns {BigNumber}
+ * @param {bigint} n
+ * @returns {bigint}
  */
 function fib_dp(n) {
-	if (n < 1) return bint(0);
-	let n2 = bint(0);
-	let n1 = bint(1);
-	let cur = bint(0);
+	if (n < 1) return 0n;
+	let n2 = 0n;
+	let n1 = 1n;
+	let cur = 0n;
 	for (let i = 1; i < n; i++) {
-		cur = n1.plus(n2);
+		cur = n1 + n2;
 		n2 = n1;
 		n1 = cur;
 	}
@@ -36,65 +35,52 @@ function fib_dp(n) {
  *
  * Space = O(1)
  * Time  = O(log n)
- * @param {BigNumber} n
- * @returns {BigNumber}
+ * @param {bigint} n
+ * @returns {bigint}
  */
 function fib_me(n) {
-	let z = [[bint(1), bint(1)], [bint(1), bint(0)]];
+	let z = [[1n, 1n], [1n, 0n]];
 
 	let result = matrix.exp(z, n);
 	return result[0][1];
 }
 
 /**
+ * Fast doubling algorithm is extracted from the matrix exponentiation algorithm.
+ * It is essentially the me algo with the duplicate calculations removed. Therefore
+ * we can use exponentiation by squaring in a more optimal way.
  *
- * @param {BigNumber} n
+ * This algorithm also works with negative values such as:
+ * n=-9  34
+ * n=-8 -21
+ * n=-7  13
+ * n=-6  -8
+ * n=-5   5
+ * n=-4  -3
+ * n=-3   2
+ * n=-2  -1
+ * n=-1   1
+ * n=0    0
+ * n=1    1
+ * n=2    1
+ * n=3    2
+ * n=4    3
+ * n=5    5
+ * n=6    8
+ *
+ * Space = O(1)
+ * Time =  O(log n) with faster constants than the ME algo
+ * @param {bigint} n
  */
-function fib_fast(n) {}
+function fib_fast(n) {
+	const vals = new Map([
+		[-2n, -1n],
+		[-1n, 1n],
+		[0n, 0n],
+		[1n, 1n],
+		[2n, 1n]
+	]); // prettier-ignore
 
-// let zero = bint(0);
-// let one = bint(1);
-// let two = bint(2);
-
-// let vals = new Map([['0', zero], ['1', one], ['2', two]]);
-
-// function fib(n) {
-// 	let v = vals.get(n.toString());
-// 	if (v) return v;
-
-// 	let k1, k2;
-
-// 	if (n.mod(2).eq(zero)) {
-// 		k1 = n.div(two);
-// 		k2 = k1.plus(one);
-// 	} else {
-// 		k1 = n.minus(one).div(two);
-// 		k2 = k1.plus(one);
-// 	}
-
-// 	let v1 = vals.get(k1.toString()) || fib(k1);
-// 	let v2 = vals.get(k2.toString()) || fib(k2);
-
-// 	if (n.mod(2).eq(zero)) v = v1.times(two.times(v2).minus(v1));
-// 	if (n.mod(2).eq(one)) v = v2.times(v2).plus(v1.times(v1));
-
-// 	vals.set(n.toString(), v);
-// 	return v;
-// }
-
-// console.time();
-// console.log(fib(bint(1000000)).toString());
-// console.timeEnd();
-
-let vals = new Map([
-	[-2n, -1n],
-	[-1n, 1n],
-	[0n, 0n],
-	[1n, 1n],
-	[2n, 1n]
-]); // prettier-ignore
-
-function fib(n) {
 	let v = vals.get(n);
 	if (v !== undefined) return v;
 
@@ -108,8 +94,8 @@ function fib(n) {
 		k2 = k1 + 1n;
 	}
 
-	let v1 = vals.get(k1) || fib(k1);
-	let v2 = vals.get(k2) || fib(k2);
+	let v1 = vals.get(k1) || fib_fast(k1);
+	let v2 = vals.get(k2) || fib_fast(k2);
 
 	if (n % 2n == 0n) v = v1 * (2n * v2 - v1);
 	else v = v2 * v2 + v1 * v1;
@@ -117,34 +103,3 @@ function fib(n) {
 	vals.set(n, v);
 	return v;
 }
-
-for (let i = 0; i < 10; i++) {
-	console.log(i, fib(BigInt(i)).toString());
-}
-
-for (let i = 0; i > -10; i--) {
-	console.log(i, fib(BigInt(i)).toString());
-}
-
-// console.time();
-// console.log(fib(2000000n).toString());
-// console.timeEnd();
-
-// f(n+2) = f(n+1) + f(n)
-
-// -9 34
-// -8 -21
-// -7 13
-// -6 -8
-// -5 5
-// -4 -3
-// -3 2
-// -2 -1
-// -1 1
-//  0 0
-//  1 1
-//  2 1
-//  3 2
-//  4 3
-//  5 5
-//  6 8
--- algorithms/test/math/fibonacci.spec.js
+++ algorithms/test/math/fibonacci.spec.js
@@ -1,30 +1,29 @@
 const { expect } = require('chai');
-const bint = require('bignumber.js');
 const fib = require('../../src/math/fibonacci');
 
 describe('fibonacci', () => {
 	describe('fib_me', () => {
 		it('n=1', () => {
-			expect(fib.fib_me(bint(1))).to.deep.equal(bint(1));
+			expect(fib.fib_me(1n)).to.deep.equal(1n);
 		});
 
 		it('n=4', () => {
-			expect(fib.fib_me(bint(4))).to.deep.equal(bint(3));
+			expect(fib.fib_me(4n)).to.deep.equal(3n);
 		});
 
 		it('n=5', () => {
-			expect(fib.fib_me(bint(5))).to.deep.equal(bint(5));
+			expect(fib.fib_me(5n)).to.deep.equal(5n);
 		});
 
 		it('n=100', () => {
-			expect(fib.fib_me(bint(100))).to.deep.equal(
-				bint('354224848179261915075')
+			expect(fib.fib_me(100n)).to.deep.equal(
+				BigInt('354224848179261915075')
 			);
 		});
 
 		it('n=1000', () => {
-			expect(fib.fib_me(bint(1000))).to.deep.equal(
-				bint(
+			expect(fib.fib_me(1000n)).to.deep.equal(
+				BigInt(
 					'43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875'
 				)
 			);
@@ -32,29 +31,65 @@ describe('fibonacci', () => {
 	});
 
 	describe('fib_fast', () => {
+		it('n=-5', () => {
+			console.log(fib.fib_fast(-5n));
+			expect(fib.fib_fast(-5n)).to.equal(5n);
+		});
+
+		it('n=-4', () => {
+			expect(fib.fib_fast(-4n)).to.equal(-3n);
+		});
+
+		it('n=-3', () => {
+			expect(fib.fib_fast(-3n)).to.equal(2n);
+		});
+
+		it('n=-2', () => {
+			expect(fib.fib_fast(-2n)).to.equal(-1n);
+		});
+
+		it('n=-1', () => {
+			expect(fib.fib_fast(-1n)).to.equal(1n);
+		});
+
+		it('n=0', () => {
+			expect(fib.fib_fast(0n)).to.equal(0n);
+		});
+
 		it('n=1', () => {
-			expect(fib.fib_fast(bint(1))).to.deep.equal(bint(1));
+			expect(fib.fib_fast(1n)).to.deep.equal(1n);
 		});
 
 		it('n=4', () => {
-			expect(fib.fib_fast(bint(4))).to.deep.equal(bint(3));
+			expect(fib.fib_fast(4n)).to.deep.equal(3n);
 		});
 
 		it('n=5', () => {
-			expect(fib.fib_fast(bint(5))).to.deep.equal(bint(5));
+			expect(fib.fib_fast(5n)).to.deep.equal(5n);
 		});
 
 		it('n=100', () => {
-			expect(fib.fib_fast(bint(100))).to.deep.equal(
-				bint('354224848179261915075')
+			expect(fib.fib_fast(100n)).to.deep.equal(
+				BigInt('354224848179261915075')
 			);
 		});
 
 		it('n=1000', () => {
-			expect(fib.fib_fast(bint(1000))).to.deep.equal(
-				bint(
+			expect(fib.fib_fast(1000n)).to.deep.equal(
+				BigInt(
 					'43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875'
 				)
+			);
+		});
+
+		xit('n=2000000', () => {
+			expect(
+				fib
+					.fib_fast(2000000n)
+					.toString()
+					.substring(0, 100)
+			).to.equal(
+				'8531294917507641543051660654503825161955433610024013070059635858838398006887605974653683126461696733'
 			);
 		});
 	});
